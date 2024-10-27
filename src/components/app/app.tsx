514689660af0b67bc0d22ba431c76de1231e19ae
antoshkaxxr <gbanton2004@yandex.ru>
2024-10-28 02:40:47 +0500

    github pages

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import {AppRoute} from "../../const/app-routes";
 import MainPage from "../../pages/main-page/main-page";
 
 function App() {
   return (
-      <BrowserRouter>
-          <Routes>
-              <Route
-                  path={AppRoute.Main}
-                  element={<MainPage />}
-              >
-              </Route>
-          </Routes>
-      </BrowserRouter>
+      <MainPage />
   )
 }
 
